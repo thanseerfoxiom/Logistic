b25d08dd37b44e900616f44556f9c6b8dd2bd3ec
thanseerfoxiom <thanseerfoxiom@gmail.com>
2025-03-11 15:42:49 +0530

    env added

--- src/services/BaseUrls.jsx
+++ src/services/BaseUrls.jsx
@@ -1,4 +1,5 @@
-export const BaseUrl = "http://192.168.1.50:3010/api/"
+// export const BaseUrl = "http://192.168.1.50:3010/api/"
+export const BaseUrl =  import.meta.env.VITE_BASE_URL
 
 export const uploadapi = "upload"
 export const userloginapi = "user/login"
--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://dev.foxiomlabs.com/xtrace',
-  //       changeOrigin: true,
-  //       secure: false,
-  //       rewrite: (path) => path.replace(/^\/api/, '/api')
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://dev.foxiomlabs.com/logistics',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
